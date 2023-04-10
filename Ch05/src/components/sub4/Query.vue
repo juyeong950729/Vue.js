0d22e71729a2d5b0cd4a1ce692915faf22e3925b
jojuyeong03 <jojuyeong03@gmail.com>
2023-04-10 17:06:49 +0900

    2023/04/10 router

---
+++ Ch05/src/components/sub4/Query.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h4>Query</h4>
+    <p>
+      user : {{ user }}<br />
+      uid : {{ user.uid }}<br />
+      name : {{ user.name }}<br />
+      age : {{ user.age }}<br />
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+// route 생성
+const route = useRoute();
+
+// query 데이터 수신
+const user = route.query;
+</script>
+
+<style scoped></style>
