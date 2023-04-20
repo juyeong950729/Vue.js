4fbe42f8d2782d975ed622c36a7ecbfcb8905528
jojuyeong03 <jojuyeong03@gmail.com>
2023-04-20 16:50:38 +0900

    2023/04/20 todoApp 수업실습

---
+++ TodoApp/src/components/Item.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <i class="fa-solid fa-check"></i>
+    <span>청소하기</span>
+    <button>삭제</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
