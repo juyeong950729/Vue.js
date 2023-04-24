9b5a987debfe9990be12a2ee76a128f7ed0e692e
jojuyeong03 <jojuyeong03@gmail.com>
2023-04-24 17:50:25 +0900

    2023/04/24 voard

---
+++ Voard/src/components/Modify.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>글수정</v-app-bar-title>
+      <v-btn @click="btnLogout">로그아웃</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-sheet max-width="800" class="mx-auto">
+          <v-text-field
+            label="제목을 입력하세요"
+            variant="underlined"
+          ></v-text-field>
+          <v-textarea label="내용을 입력하세요" variant="outlined"></v-textarea>
+          <v-file-input label="File input" variant="underlined"></v-file-input>
+          <v-sheet class="text-center">
+            <v-btn class="mr-2" @click="">취소</v-btn>
+            <v-btn color="primary" @click="">등록</v-btn>
+          </v-sheet>
+        </v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ Voard/src/components/View.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>글보기</v-app-bar-title>
+      <v-btn @click="btnLogout">로그아웃</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-sheet max-width="800" class="mx-auto">
+          <v-text-field
+            color="#111"
+            label="제목"
+            variant="underlined"
+            readonly
+          ></v-text-field>
+          <v-textarea label="내용" variant="outlined" readonly></v-textarea>
+          <v-file-input
+            label="File input"
+            variant="underlined"
+            readonly
+          ></v-file-input>
+          <v-sheet class="text-center">
+            <v-btn class="mr-2" @click="">수정</v-btn>
+            <v-btn color="primary" @click="">목록</v-btn>
+          </v-sheet>
+        </v-sheet>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
